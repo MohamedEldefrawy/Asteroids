ebfdc19813f6e8fd181bccda53520744eacd3f4a
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-11-03 01:56:41 +0200

    Creaate Ship sprite and Drive the Ship using Ship script

---
+++ Asteroids/Assets/Scripts/Ship.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ship Configurations
+/// </summary>
+public class Ship : MonoBehaviour {
+
+    #region Fields
+    Rigidbody2D rigidbody2;
+    Vector2 thrustDirection ;
+    #endregion
+    
+    #region Constants
+    const float ThrustForce = 1;
+    #endregion
+
+    #region Methods
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+        thrustDirection = new Vector2(1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Used to apply Force to RigidBody
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            rigidbody2.AddForce(thrustDirection * ThrustForce,ForceMode2D.Force);
+
+        }
+    }
+    #endregion
+}
