ac81ff393fb31dec7e1aea5f51b12ace90f82fe0
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-11-03 11:25:23 +0200

    Rotate the Ship When getting out of gameWorld boundries

--- Asteroids/Assets/Scripts/Ship.cs
+++ Asteroids/Assets/Scripts/Ship.cs
@@ -10,6 +10,7 @@ public class Ship : MonoBehaviour {
     #region Fields
     Rigidbody2D rigidbody2;
     Vector2 thrustDirection ;
+    CircleCollider2D circleCollider2D;
     #endregion
     
     #region Constants
@@ -17,10 +18,12 @@ public class Ship : MonoBehaviour {
     #endregion
 
     #region Methods
+
     // Use this for initialization
     void Start()
     {
         rigidbody2 = GetComponent<Rigidbody2D>();
+        circleCollider2D = GetComponent<CircleCollider2D>();
         thrustDirection = new Vector2(1, 0);
     }
 
@@ -37,6 +40,35 @@ public class Ship : MonoBehaviour {
         {
             rigidbody2.AddForce(thrustDirection * ThrustForce,ForceMode2D.Force);
 
+        }
+    }
+
+    // Wrap Ship When getting out of camera boundries
+    private void OnBecameInvisible()
+    {
+        if ((circleCollider2D.transform.position.x - circleCollider2D.radius) >= ScreenUtils.ScreenRight)
+        {
+            Vector2 pos = transform.position;
+            pos.x = ScreenUtils.ScreenLeft;
+            transform.position = pos;
+        }
+        else if((circleCollider2D.transform.position.x - circleCollider2D.radius) <= ScreenUtils.ScreenLeft)
+        {
+            Vector2 pos = transform.position;
+            pos.x = ScreenUtils.ScreenRight;
+            transform.position = pos;
+        }
+        else if((circleCollider2D.transform.position.y - circleCollider2D.radius) > ScreenUtils.ScreenTop)
+        {
+            Vector2 pos = transform.position;
+            pos.y = ScreenUtils.ScreenBottom;
+            transform.position = pos;
+        }
+        else if ((circleCollider2D.transform.position.y - circleCollider2D.radius) < ScreenUtils.ScreenBottom)
+        {
+            Vector2 pos = transform.position;
+            pos.y = ScreenUtils.ScreenTop;
+            transform.position = pos;
         }
     }
     #endregion
