328b6e0194f979f364cc83e6419784be3052a169
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-11-08 15:39:08 +0200

    Making a moving asteroid with random sprite

--- Asteroids/Assets/Scripts/Asteroid.cs
+++ Asteroids/Assets/Scripts/Asteroid.cs
@@ -1,25 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
-/// Configure Asteroids
+/// Configure Asteroids (Ships)
 /// </summary>
-public class Asteroid : MonoBehaviour {
-
-    private float forceMagnitude = 1;
+public class Asteroid : MonoBehaviour
+{
+    #region Private memmbers
+    private float forceMagnitude;
     private float forceDirectionRange;
     private Vector2 forceDirection;
-	// Use this for initialization
-	void Start () {
+    #endregion
+
+    #region Serialized memmbers
+    [SerializeField]
+    private Sprite[] shipsSprites;
+    #endregion
+
+    #region Methods
+
+    // Use this for initialization
+    private void Start()
+    {
+        #region Moving the Ship
+        // Create random force variable
         forceMagnitude = Random.Range(1, 5);
+
+        // Create random direction from 0 to 2 pi
         forceDirectionRange = Random.Range(0, Mathf.Deg2Rad * 360);
+
+        // Craete Vector2 to hold the direction
         forceDirection = new Vector2(Mathf.Cos(forceDirectionRange), Mathf.Sin(forceDirectionRange));
-        transform.GetComponent<Rigidbody2D>().AddForce((forceMagnitude*forceDirection),ForceMode2D.Impulse);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        // Adding inpulse force with random force and radom direction
+        transform.GetComponent<Rigidbody2D>().AddForce((forceMagnitude * forceDirection), ForceMode2D.Impulse);
+        #endregion
+
+        // Select Random Sprite for the Ship
+        gameObject.GetComponent<SpriteRenderer>().sprite = shipsSprites[Random.Range(0, shipsSprites.Length)];
+    }
+    #endregion
 }
