5ac320b19dc040188e6f5cb5a782fd0eb90cf9a7
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-11-08 15:19:55 +0200

    Adding more sprites and create a Asteroid Script Which moving the ship with random force and direction

---
+++ Asteroids/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Configure Asteroids
+/// </summary>
+public class Asteroid : MonoBehaviour {
+
+    private float forceMagnitude = 1;
+    private float forceDirectionRange;
+    private Vector2 forceDirection;
+	// Use this for initialization
+	void Start () {
+        forceMagnitude = Random.Range(1, 5);
+        forceDirectionRange = Random.Range(0, Mathf.Deg2Rad * 360);
+        forceDirection = new Vector2(Mathf.Cos(forceDirectionRange), Mathf.Sin(forceDirectionRange));
+        transform.GetComponent<Rigidbody2D>().AddForce((forceMagnitude*forceDirection),ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
